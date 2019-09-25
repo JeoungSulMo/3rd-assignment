9dc17ad33b8d89360e3e7b7f3e43241d9b4f5bd2
sulmo <tjdah0853@gmail.com>
2019-09-25 21:36:48 +0900

    load movie list

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-window.onload = async () => {
+window.onload = function() {
 	const app = document.querySelector("#app");
 	const loading = document.createElement("p");
 	loading.innerText = "Loading...";
 	app.appendChild(loading);
-	const main = async () => {
-		const {
-			data: {
-				data: { movie: data }
-			}
-		} = await axios.get(
-			"https://yts.lt/api/v2/movie_details.json?movie_id=2019"
-		);
+		
 
-		console.log(data);
+	function makeMovieList(object){
+		app.removeChild(loading);
+		for(i=0;i<20;i++){
 		const title = document.createElement("p");
 		const img = document.createElement("img");
 		const desc = document.createElement("p");
-		title.innerText = data.title_long;
-		img.src = data.medium_cover_image;
-		desc.innerText = data.description_full;
-		const card = document.createElement("p");
+		const movieTitle = object.data.movies[i].title_long;
+		const movieImg = object.data.movies[i].medium_cover_image;
+		const movieContent = object.data.movies[i].description_full;
+		title.innerText = `${movieTitle}`;
+		img.src = `${movieImg}`;
+		desc.innerText = `${movieContent}`;
+		const card = document.createElement("div");
 		card.append(img, title, desc);
-		app.removeChild(loading);
-		app.appendChild(card);
-	};
+		
+		app.appendChild(card);	
+		}
+		
+	}
+	function fetching(){
+		fetch(`https://yts.lt/api/v2/list_movies.json?quality=3D`).then(function(response){
+			return response.json()
+			}).then(function(json){
+				const movieData = json;
+				console.log(json);
+				makeMovieList(movieData);
+			});		
+	}
 
-	await main();
-};
+	function loadData(){
+		// for(i=0 ; i<20; i++){
+			 
+			fetching();
+			
+		// }
+	}
+	
+	
+	function init(){
+		loadData();
+	}
+	
+	init();
+}
